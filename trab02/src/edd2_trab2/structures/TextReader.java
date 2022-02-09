4397b07d5f9df2a34fe1b52c2f7ce46d551d0adb
Thiago Sousa <thiagosilvadesousa@edu.unirio.br>
2022-02-09 18:33:10 -0300

    comment TextReader

--- trab02/src/edd2_trab2/structures/TextReader.java
+++ trab02/src/edd2_trab2/structures/TextReader.java
@@ -9,17 +9,17 @@ import java.util.List;
 public class TextReader{
 
     public String textRead(String caminho){
-        Path path = Paths.get(caminho);
-        String texto = "";
+        Path path = Paths.get(caminho); //Recebe um diretório no formato de string e convert num Path utilizavel
+        String texto = ""; //declara a variável que mais tarde irá comportar o texto
 
-        List<String> linhasArquivo = null;
+        List<String> linhasArquivo = null; //declara a lista que vai receber as linhas do texto
         try {
-            linhasArquivo = Files.readAllLines(path);
+            linhasArquivo = Files.readAllLines(path); //a lista recebe as linhas do texto
         } catch (IOException e) {
             e.printStackTrace();
         }
         for (String linha : linhasArquivo) {
-            texto += linha;
+            texto += linha; //para cada linha do texto a variavel "texto" vai concatenar com o que já estiver nela
         }
         return texto;
     }
